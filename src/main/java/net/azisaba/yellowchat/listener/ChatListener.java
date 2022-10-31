d0ea239033787ad406da2b3a86e21fcbc6051bd1
acrylic-style <me@acrylicstyle.xyz>
2022-10-31 17:16:34 +0900

    Set the event priority to LOW

--- src/main/java/net/azisaba/yellowchat/listener/ChatListener.java
+++ src/main/java/net/azisaba/yellowchat/listener/ChatListener.java
@@ -7,7 +7,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
 public class ChatListener implements Listener {
-    @EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
+    @EventHandler(priority = EventPriority.LOW, ignoreCancelled = true)
     public void onChat(AsyncPlayerChatEvent e) {
         if (!YellowChat.getInstance().getExcludedPlayers().contains(e.getPlayer().getUniqueId()) &&
                 e.getPlayer().hasPermission("yellowchat.use")) {
